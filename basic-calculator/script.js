a3b0674fbe5f8da8bbfbb0a3e88e4c8246edadf0
Jack <notnowjack67@gmail.com>
2025-01-16 17:02:33 +0000

    Completed part 3

--- basic-calculator/script.js
+++ basic-calculator/script.js
@@ -3,3 +3,34 @@ function getNumbers() {
   const num2 = parseFloat(document.getElementById('num2').value);
   return {num1, num2};
 }
+
+function calculate(operation) {
+    const {num1, num2} = getNumbers();
+    let result;
+
+    if (isNaN(num1) || isNaN(num2)) {
+        return "Enter valid numbers";
+    }
+
+    switch (operation) {
+        case "add":
+            result = num1 + num2;
+            break;
+        case "subtract":
+            result = num1 - num2;
+            break;
+        case "multiply":    
+            result = num1 * num2;
+            break;
+        case "divide":  
+            if (num2 === 0) {
+                return "Cannot divide by zero";
+            }
+            result = num1 / num2;
+            break;
+        default:
+            return "Invalid operation";
+    }
+
+    return result;
+}
