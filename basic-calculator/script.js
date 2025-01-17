809f2edd47ce3f2b1c0fe8c2dfc2b855f2d6ab8b
Jack <notnowjack67@gmail.com>
2025-01-17 09:37:49 +0000

    added history

--- basic-calculator/script.js
+++ basic-calculator/script.js
@@ -8,8 +8,8 @@ function calculate(operation) {
     const {num1, num2} = getNumbers();
     let result;
 
-    if (isNaN(num1) || isNaN(num2)) {
-        return "Enter valid numbers";
+    if (isNaN (num1) || isNaN(num2)) {
+        return "Invalid number(s)";
     }
 
     switch (operation) {
@@ -35,18 +35,44 @@ function calculate(operation) {
     return result;
 }
 
+function addCalculation(calcResult) {
+    const {num1, num2} = getNumbers();
+    equation = `${num1} + ${num2} = ${calcResult}`;
+
+    const newCalculation = document.createElement("li");
+    newCalculation.innerText = equation;
+    document.getElementById("calculations").appendChild(newCalculation);
+}
+
+function clearPage() {
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("result").textContent = "Result:";
+}
+
 document.getElementById("add").addEventListener("click", () => {
     document.getElementById("result").textContent = `Result: ${calculate('add')}`;
 })
+document.getElementById("add").addEventListener("click", () => addCalculation(calculate('add')));
+
 
 document.getElementById("subtract").addEventListener("click", () => {
     document.getElementById("result").textContent = `Result: ${calculate('subtract')}`;
 })
+document.getElementById("subtract").addEventListener("click", () => addCalculation(calculate('subtract')));
+
 
 document.getElementById("multiply").addEventListener("click", () => {
     document.getElementById("result").textContent = `Result: ${calculate('multiply')}`;
 })
+document.getElementById("multiply").addEventListener("click", () =>  addCalculation(calculate('multiply')));
+
 
 document.getElementById("divide").addEventListener("click", () => {
     document.getElementById("result").textContent = `Result: ${calculate('divide')}`;
 })
+document.getElementById("divide").addEventListener("click", () => addCalculation(calculate('divide')));
+
+
+document.getElementById("clear").addEventListener("click", clearPage);
+
